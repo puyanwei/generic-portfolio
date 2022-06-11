b60e920d851aa54414621d33497fb6b6801c3ee2
Puyan Wei <puyanwei@gmail.com>
2022-06-11 11:20:21 +0100

    Tailwind config to work with Storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    content: [`./src/pages/**/*.{js,ts,jsx,tsx}`, `./src/components/**/*.{js,ts,jsx,tsx}`,`./src/**/*.{js,ts,jsx,tsx}`],
-    theme: {
+  content: [`./src/**/*.ts`, `./src/**/*.tsx`],
+  theme: {
     extend: {},
   },
   plugins: [],
