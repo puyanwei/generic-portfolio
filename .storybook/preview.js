6da5194692c57bae76c34f9e46b6045eb6e5786d
Puyan Wei <puyanwei@gmail.com>
2022-07-04 23:26:55 +0100

    Config force ordering of stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,4 +8,24 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  options: {
+    storySort: {
+      order: [
+        `Universe`,
+        `Galaxy`,
+        [
+          `Header`,
+          `Hero Section`,
+          `Project Gallery Section`,
+          `About Me Section`,
+          `Blog Feed Section`,
+          `Contact Us Section`,
+          `Footer`,
+        ],
+        `Solar System`,
+        `Planet`,
+        `Black Hole`,
+      ],
+    },
+  },
 }
