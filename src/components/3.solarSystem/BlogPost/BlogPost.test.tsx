0a9f320bc63c51f106ea6f27409e05b6426d0a23
Puyan Wei <puyanwei@gmail.com>
2022-07-04 21:57:43 +0100

    Added tests

--- src/components/3.solarSystem/BlogPost/BlogPost.test.tsx
+++ src/components/3.solarSystem/BlogPost/BlogPost.test.tsx
@@ -8,17 +8,26 @@ describe(`BlogPost`, () => {
     expect(screen.getByTestId(`blog-post`)).toBeVisible()
   })
 
-  it(`renders the children correctly`, () => {
-    render(<BlogPost {...blogPostMockProps}>Hello world</BlogPost>)
-    expect(screen.getByText(`Hello world`)).toBeVisible()
-  })
-
   it(`className default prop should be blank string`, () => {
     render(<BlogPost {...blogPostMockProps} />)
     expect(screen.getByTestId(`blog-post`)).not.toHaveClass(`undefined`)
   })
 
-  it.todo(`should render the title of the blogposts`)
-  it.todo(`should render the date of the blogposts`)
-  it.todo(`should add the url link to the blogpost title`)
+  it(`should render the title and date of the blogposts`, () => {
+    render(
+      <BlogPost
+        {...blogPostMockProps}
+        date={`12th March 2022`}
+        title={`How I am so productive by waking up at 2am`}
+      />
+    )
+    expect(screen.getByText(`12th March 2022`)).toBeVisible()
+    expect(
+      screen.getByText(`How I am so productive by waking up at 2am`)
+    ).toBeVisible()
+  })
+  it(`should add the url link to the blogpost title`, () => {
+    render(<BlogPost {...blogPostMockProps} url="/contact-us" />)
+    expect(screen.getByTestId(`link`)).toHaveAttribute(`href`, `/contact-us`)
+  })
 })
