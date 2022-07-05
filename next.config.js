4a9863cdcecdacbaef2be030bc41a41a9589bed7
Puyan Wei <puyanwei@gmail.com>
2022-07-05 21:35:45 +0100

    Update Next to experimental version where Next/Image isn't stupid

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: [`images.unsplash.com`],
   },
+  experimental: {
+    images: {
+      layoutRaw: true,
+    },
+  },
 }
 
 module.exports = nextConfig
