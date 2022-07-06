9b7fe1fa762ce5049389eddd6f2164cfa064d03a
Puyan Wei <puyanwei@gmail.com>
2022-07-06 18:50:47 +0100

    Updated links for blog feed

--- src/components/2.galaxy/BlogFeedSection/BlogFeedSection.mockProps.ts
+++ src/components/2.galaxy/BlogFeedSection/BlogFeedSection.mockProps.ts
@@ -5,21 +5,21 @@ export const blogFeedSectionMockProps: BlogFeedData[] = [
   {
     date: `Yesterday`,
     title: `How do you make a site faster?`,
-    url: `/how-do-you-make-a-site-faster`,
+    url: `https://bit.ly/3RcCj6l`,
   },
   {
     date: `13th June`,
     title: `I wake up at 2am and so should you`,
-    url: `/I-wake-up-at-2am-and-so-should-you`,
+    url: `https://bit.ly/3nIOt9F`,
   },
   {
     date: `14th June`,
     title: `100 projects in 100 days`,
-    url: `/100-projects-in-100-days`,
+    url: `https://bit.ly/3ImEi4g`,
   },
   {
     date: `15th June`,
     title: `Code with your left hand only challenge`,
-    url: `/Code-with-your-left-hand-only-challenge`,
+    url: `https://bit.ly/3yKGrDq`,
   },
 ]
